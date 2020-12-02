30153ea210e29542118bdb49b56fa9aa9d6976fa
nhandrew <fourjulows@yahoo.com>
2020-12-02 15:07:56 -0500

    Update references for new firestore syntax

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firestore_crud/providers/product_provider.dart';
 import 'package:firestore_crud/screens/products.dart';
 import 'package:firestore_crud/services/firestore_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -2,18 +2,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firestore_crud/models/product.dart';
 
 class FirestoreService {
-  Firestore _db = Firestore.instance;
+  FirebaseFirestore _db = FirebaseFirestore.instance;
 
   Future<void> saveProduct(Product product){
-    return _db.collection('products').document(product.productId).setData(product.toMap());
+    return _db.collection('products').doc(product.productId).set(product.toMap());
   }
 
   Stream<List<Product>> getProducts(){
-    return _db.collection('products').snapshots().map((snapshot) => snapshot.documents.map((document) => Product.fromFirestore(document.data)).toList());
+    return _db.collection('products').snapshots().map((snapshot) => snapshot.docs.map((document) => Product.fromFirestore(document.data())).toList());
   }
 
   Future<void> removeProduct(String productId){
-    return _db.collection('products').document(productId).delete();
+    return _db.collection('products').doc(productId).delete();
   }
 
 
